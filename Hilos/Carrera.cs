7b399379454bd1da9483cb9d2c430e7e1b240cfc
ERICK-DC <67754832+ERICK-DC@users.noreply.github.com>
2021-01-11 14:01:12 -0500

    hilos

---
+++ Hilos/Carrera.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace Hilos
+{
+    class Carrera 
+    {
+        public static void Corredor(object obj) 
+        {
+            var nombre = (string)obj;
+            //var hiloActual = Thread.CurrentThread;
+            var pasos = 0;
+            var random = new Random();
+            while (pasos < 10) 
+            {
+                pasos += random.Next(0,4);
+                for (int i = 0; i <= pasos; i++) 
+                {
+                    Console.WriteLine("*");
+                }
+                    Console.WriteLine("Corredor {0} dio {1} pasos", nombre, pasos);
+                    Thread.Sleep(10);
+            }
+            Console.WriteLine("El corredor {0} llego a la meta ", nombre);
+        }
+    }
+    //dibujar columnas simulando que seas corredores  \\\ 
+}
